d866ae5ecc69cc2bda2fe8e66114419561e7140d
wmliu <AcceleratorWayne@outlook.com>
2024-04-08 17:59:04 +0800

    新增用户服务模块，shiro模块只做授权

---
+++ escanor-user-service/src/main/java/com/escanor/user/controller/UserController.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2024 Weimeng Liu
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.escanor.user.controller;
+
+import com.escanor.jpa.utils.ModelMapperUtils;
+import com.escanor.user.dto.UserInfoDto;
+import com.escanor.user.service.UserInfoService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    final UserInfoService userInfoService;
+
+    public UserController(UserInfoService userInfoService) {
+        this.userInfoService = userInfoService;
+    }
+
+    @GetMapping("/findByUserName/{userName}")
+    public UserInfoDto findByUserName(@PathVariable("userName") String userName) {
+        return ModelMapperUtils.map(userInfoService.findByUserName(userName), UserInfoDto.class);
+    }
+
+}
